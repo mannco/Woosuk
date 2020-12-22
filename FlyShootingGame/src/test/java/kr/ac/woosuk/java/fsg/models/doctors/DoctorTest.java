ef8101e250ba47e5d634968614b5a15111d697df
k01km <k01km@DESKTOP-0B68KO2>
2020-12-22 21:42:18 +0900

    src/main/java , src/test/java 로 나누고 DoctorTest, EnemyTest 테스트클래스 추가했습니다.

---
+++ FlyShootingGame/src/test/java/kr/ac/woosuk/java/fsg/models/doctors/DoctorTest.java
@@ -0,0 +1,14 @@
+package kr.ac.woosuk.java.fsg.models.doctors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DoctorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ FlyShootingGame/src/test/java/kr/ac/woosuk/java/fsg/models/enemies/EnemyTest.java
@@ -0,0 +1,14 @@
+package kr.ac.woosuk.java.fsg.models.enemies;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EnemyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

a0e93d3b212be8d611eff1089a416ba17b82b191
coldbrewlatte5252 <coldbrewlatte5252@gmail.com>
2020-12-28 23:22:49 +0900

    에너미샷 추가
    에너미샷 추가

---
+++ FlyShootingGame/src/test/java/kr/ac/woosuk/java/fsg/models/enemyShot/GermShotTest.java
@@ -0,0 +1,39 @@
+package kr.ac.woosuk.java.fsg.models.enemyShot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kr.ac.woosuk.java.fsg.models.doctors.Doctor;
+import kr.ac.woosuk.java.fsg.models.doctors.DoctorRed;
+import kr.ac.woosuk.java.fsg.models.enemies.Enemy;
+import kr.ac.woosuk.java.fsg.models.enemies.Virus;
+import kr.ac.woosuk.java.fsg.models.enemyshots.EnemyShot;
+import kr.ac.woosuk.java.fsg.models.enemyshots.GermShot;
+import kr.ac.woosuk.java.fsg.models.enemyshots.VirusShot;
+
+class GermShotTest {
+
+	@Test
+	void 공격테스트() {
+		Enemy enemy = new Virus();	//바이러스 생성
+		
+		EnemyShot enemyShot = new GermShot();	//발사체 생성
+		assertEquals(1, enemyShot.getAttackpoint());	//발사체 공격력 확인
+		
+		Doctor doctor = new DoctorRed();	//의사 생성
+		assertEquals(3, doctor.getLifepoint());	//객체 체력 확인
+		
+		enemyShot.AttackDoctor(doctor);	//세균 의사 공격
+		
+		assertEquals(2, doctor.getLifepoint());
+	}
+	
+	@Test
+	void 움직임테스트() {
+		EnemyShot enemyShot = new GermShot();	//세균샷 생성
+		enemyShot.moveDown();	//아래로 움직임
+		assertEquals(1, enemyShot.getY());
+	}
+
+}

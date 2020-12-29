eb538a0e1d44fcee95f264ef4c09d78196afe4ab
bluedog129 <bluedog129@gmail.com>
2020-12-29 16:45:11 +0900

    적소멸 추가

--- FlyShootingGame/src/main/java/kr/ac/woosuk/java/fsg/controllers/Controller.java
+++ FlyShootingGame/src/main/java/kr/ac/woosuk/java/fsg/controllers/Controller.java
@@ -7,16 +7,47 @@ import kr.ac.woosuk.java.fsg.models.doctors.Doctor;
 import kr.ac.woosuk.java.fsg.models.enemies.Enemy;
 
 public class Controller {
+    private Doctor doctors;
+    private List<Enemy> enemies;
 
-	private Doctor doctors;
-	private List<Enemy> enemies;
-	
-	public void addDoctor(Doctor doctor) {
-		this.doctors=doctor;
-	}
+    public void setDoctors(Doctor doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public void setEnemies(List<Enemy> enemies) {
+        this.enemies = enemies;
+    }
 
 
-	public void addEnemy(Enemy enemy) {
-		this.enemies = new ArrayList<Enemy>();
-	}
+
+    public void addDoctor(Doctor doctor) {
+        // TODO Auto-generated method stub
+        this.doctors = doctor;
+    }
+
+    public void addEnemy(Enemy enemy) {
+        // TODO Auto-generated method stub
+        this.enemies = new ArrayList<Enemy>();
+    }
+
+    public void removeDoctor() {
+        // TODO Auto-generated method stub
+        this.doctors = null;
+    }
+
+    public Doctor getDoctors() {
+        // TODO Auto-generated method stub
+        return doctors;
+    }
+
+    public void removeEnemy(Enemy enemy) {
+        // TODO Auto-generated method stub
+        this.enemies.remove(enemy);
+    }
+
+
 }
--- FlyShootingGame/src/test/java/kr/ac/woosuk/java/fsg/controllers/ControllerTest.java
+++ FlyShootingGame/src/test/java/kr/ac/woosuk/java/fsg/controllers/ControllerTest.java
@@ -2,7 +2,6 @@ package kr.ac.woosuk.java.fsg.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import org.junit.jupiter.api.Test;
 
 import kr.ac.woosuk.java.fsg.models.doctors.Doctor;
@@ -10,43 +9,51 @@ import kr.ac.woosuk.java.fsg.models.doctors.DoctorRed;
 import kr.ac.woosuk.java.fsg.models.enemies.Enemy;
 import kr.ac.woosuk.java.fsg.models.enemies.Virus;
 
-
 class ControllerTest {
 
-	@Test
-	void 닥터생성() {
-		Controller controller = new Controller();
-		//닥터레드 생성
-		Doctor doctor = new DoctorRed();
-		//컨트롤러에 닥터 생성
-		controller.addDoctor(doctor);
-		//테스트
-		assertNotNull(doctor);
-	}
-	
-	@Test
-	void 적생성() {
-		Controller controller = new Controller();
-		//적 생성
-		Enemy enemy = new Virus();
-		//컨트롤러에 적 생성
-		controller.addEnemy(enemy);
-		//테스트
-		assertNotNull(enemy);
-	} 
-	
-	@Test
-	void 닥터소멸 () {
-		Controller controller = new Controller();
-		//닥터생성
-		Doctor doctor = new DoctorRed();
-		//컨트롤러에 닥터 생성
-		Enemy enemy = new Virus();
-		//컨트롤러에 적 생성
-		controller.addDoctor(doctor);
-		//컨트롤러에 있는 닥터 소멸
-		
-	}
-	
-	
+    @Test
+    void 닥터생성() {
+        Controller controller = new Controller();
+        Doctor doctor = new DoctorRed();
+        controller.addDoctor(doctor);
+
+        assertNotNull(controller.getDoctors());
+
+    }
+
+    @Test
+    void 애너미생성() {
+        Controller controller = new Controller();
+        Enemy enemy = new Virus();
+        controller.addEnemy(enemy);
+
+        assertNotNull(controller.getEnemies());
+
+    }
+    @Test
+    void 닥터소멸() {
+        Controller controller = new Controller();
+        Doctor doctor = new DoctorRed();
+        controller.addDoctor(doctor);
+        assertNotNull(controller.getDoctors());
+
+        controller.removeDoctor();
+
+        assertNull(controller.getDoctors());
+
+    }
+
+    @Test
+    void 적소멸() {
+        Controller controller = new Controller();
+        Enemy enemy = new Virus();
+        controller.addEnemy(enemy);
+
+        assertNotNull(controller.getEnemies());
+
+        controller.removeEnemy(enemy);
+
+        assertNull(controller.getEnemies());
+
+    }
 }

90a096ba75647556b583816834dd76d3dd6b8fb0
bluedog129 <bluedog129@gmail.com>
2020-12-29 16:08:24 +0900

    컨트롤러테스트 / 닥터생성, 적생성 추가 2

---
+++ FlyShootingGame/src/test/java/kr/ac/woosuk/java/fsg/controllers/ControllerTest.java
@@ -0,0 +1,37 @@
+package kr.ac.woosuk.java.fsg.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+import kr.ac.woosuk.java.fsg.models.doctors.Doctor;
+import kr.ac.woosuk.java.fsg.models.doctors.DoctorRed;
+import kr.ac.woosuk.java.fsg.models.enemies.Enemy;
+import kr.ac.woosuk.java.fsg.models.enemies.Virus;
+
+
+class ControllerTest {
+
+	@Test
+	void 닥터생성() {
+		Controller controller = new Controller();
+		//닥터레드 생성
+		Doctor doctor = new DoctorRed();
+		//컨트롤러에 닥터 생성
+		controller.addDoctor(doctor);
+		//테스트
+		assertNotNull(doctor);
+	}
+	
+	@Test
+	void 적생성() {
+		Controller controller = new Controller();
+		//적 생성
+		Enemy enemy = new Virus();
+		//컨트롤러에 적 생성
+		controller.addEnemy(enemy);
+		//테스트
+		assertNotNull(enemy);
+	}
+}
